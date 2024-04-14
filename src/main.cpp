859ecd1a79ada485a00315bbf8cd590a6500111e
Kori Miyohashi <miyohashikori457@gmail.com>
2024-04-14 20:00:11 +0800

    finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,106 @@
 #include <WiFi.h>
-#include "control.h"
+#include <WebServer.h>
+
+constexpr int L1 = 6;  // Yellow
+constexpr int L2 = 10; // Orange
+constexpr int R1 = 18; // Green
+constexpr int R2 = 19; // Gray
+
+constexpr int DELAY = 1000;
 
 const char *ssid = "ESP32";
 const char *password = "12345678";
 
-WiFiServer server(80);
+WebServer server(80);
 
-enum State
+void control(const char *cmd)
 {
-  WAITING = 0,
-  CONNECTED
-};
-
-State state = WAITING;
+  if (strcmp(cmd, "CTRL U") == 0)
+  {
+    digitalWrite(L1, HIGH);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, HIGH);
+    digitalWrite(R2, LOW);
+    delay(DELAY);
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+  else if (strcmp(cmd, "CTRL D") == 0)
+  {
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, HIGH);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, HIGH);
+    delay(DELAY);
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+  else if (strcmp(cmd, "CTRL L") == 0)
+  {
+    digitalWrite(L1, HIGH);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, HIGH);
+    delay(DELAY);
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+  else if (strcmp(cmd, "CTRL R") == 0)
+  {
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, HIGH);
+    digitalWrite(R1, HIGH);
+    digitalWrite(R2, LOW);
+    delay(DELAY);
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+  else if (strcmp(cmd, "CTRL S") == 0)
+  {
+    digitalWrite(L1, LOW);
+    digitalWrite(L2, LOW);
+    digitalWrite(R1, LOW);
+    digitalWrite(R2, LOW);
+  }
+}
 
 void setup()
 {
+  pinMode(12, OUTPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
   Serial.begin(115200);
   WiFi.softAP(ssid, password);
 
-  Serial.println("SoftAP started");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.softAPIP());
+  server.on("/heart", HTTP_GET, []()
+            {
+      digitalWrite(12, HIGH);
+      server.send(200, "text/plain", "heart");
+      digitalWrite(12, LOW); });
+
+  server.on("/move", HTTP_POST, []()
+            {
+    String data = server.arg("plain");
+    if(data.startsWith("CTRL"))
+    {
+      control(data.c_str());
+    }
+    server.send(200, "text/plain","ok"); });
 
   server.begin();
-  Serial.println("Server started");
 }
 
 void loop()
 {
-  Serial.println("Waiting for client connection...");
-
-  WiFiClient client = server.available();
-
-  if (client)
-  {
-    Serial.println("Client connected");
-    state = CONNECTED;
-  }
-
-  while (state == CONNECTED)
-  {
-    if (client.available())
-    {
-      String cmd = client.readStringUntil('\n');
-      if (cmd.startsWith("CTRL"))
-      {
-
-        ctrl::control(cmd.substring(5).c_str());
-      }
-      if (cmd.startsWith("OFF"))
-      {
-        client.stop();
-        state = WAITING;
-      }
-    }
-  }
+  server.handleClient();
 }
